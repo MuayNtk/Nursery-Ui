9f25b2235e0cebad45a908b9b937aa23c81341d9
MuayNtk <natthakarn.waa789@gmail.com>
2024-06-17 21:47:11 +0700

    update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,7 +6,7 @@ const Footer: React.FC = () => {
     // <footer className="bottom-0 w-full  bg-white border-t border-gray-200 dark:bg-gray-800 dark:border-gray-700">
     <footer className=" overflow-y-auto text-sky-900 py-2 bottom-0 md:ml-56 flex justify-center">
         <div className="text-center">
-            <p>&copy; {new Date().getFullYear()} Your Company Name. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Japansystem.</p>
         </div>
     </footer>
 
