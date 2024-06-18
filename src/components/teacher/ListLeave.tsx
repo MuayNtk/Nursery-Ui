b52260eecc47ae0b7efd83bb839ead607e07a93b
NTKM10 <muaysiam48@gmail.com>
2024-06-18 15:05:37 +0700

    add listleave edit route

---
+++ src/components/teacher/ListLeave.tsx
@@ -0,0 +1,15 @@
+import ContentMain from '../content/Content';
+
+
+export default function ListLeave() {
+
+ 
+    return (
+        <>
+        <ContentMain>
+       
+          <h1>ListLeave</h1>
+        </ContentMain>
+        </>
+      );
+    };
---
+++ src/pages/teacher/ListLeavePages.tsx
@@ -0,0 +1,8 @@
+import ListLeave from "../../components/teacher/ListLeave";
+
+
+export default function ListLeavePages() {
+  return <ListLeave />;
+}
+
+
