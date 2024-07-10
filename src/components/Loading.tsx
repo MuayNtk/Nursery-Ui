fec877339a2277b0e72a4516bff74e532280436d
NTKM10 <muaysiam48@gmail.com>
2024-07-10 15:54:01 +0700

    add

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,7 +1,7 @@
 
 export default function Loading() {
       return (
-            <div className="flex-1 overflow-y-auto p-4 bg-slate-50 md:ml-72 mt-2 md:mr-2 mb-2 min-h-screen flex items-center justify-center">
+            <div className="flex-1 overflow-y-auto p-4 bg-slate-50 md:ml-64 mt-2 md:mr-2 mb-2 min-h-screen flex items-center justify-center">
                   <div className="flex space-x-2 animate-pulse">
                         <div className="w-3 h-3  bg-sky-800 rounded-full"></div>
                         <div className="w-3 h-3  bg-sky-600 rounded-full"></div>
